cd5a249a15203c281bae32aa7f07ddf0cb51bd58
Nguyen Viet Hoang (APTECH HN) <hoangnvth2010033@fpt.edu.vn>
2022-09-01 15:39:26 +0700

    Set up MongoDB

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import { connectDB } from "./config/mongodb";
+
 import { mapOrder } from '*/utilities/sorts.js'
 
 const app = express()
@@ -7,10 +9,12 @@ const app = express()
 const hostname = 'localhost'
 const port = 8017
 
+connectDB().catch(console.log)
+
 app.get('/', (req, res) => {
     res.end('<h1>Hello </h1> <hr/>')
 })
 
 app.listen(port, hostname, () => {
-   console.log('Hi there')
+   console.log(`Hi there `)
 })
